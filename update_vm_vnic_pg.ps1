c3d3ed02a743c728bf7efc5b47529c5f623586fe
Shane Delima <114932462+shane25p@users.noreply.github.com>
2022-11-06 21:50:00 +0000

    Update update_vm_vnic_pg.ps1

--- update_vm_vnic_pg.ps1
+++ update_vm_vnic_pg.ps1
@@ -1,1 +1,8 @@
+$oldpg = "VM Network_VDS"
+$newpg = "vSS.VM Network"
+
+ForEach ($vms in (Get-Content -Path C:\VMs.csv))
+{
+Get-VM -Name $vms | Get-NetworkAdapter | where {$_.NetworkName -eq "$oldpg"} | Set-NetworkAdapter -NetworkName $newpg -Confirm:$false
+} 
 
